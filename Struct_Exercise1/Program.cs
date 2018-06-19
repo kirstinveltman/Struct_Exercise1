8c06724e5470ac07f0feb9530eb4e0398bee667b
Kirstin Veltman <kirstin@veltman.co>
2018-06-19 15:09:23 -0700

    Add project files.

---
+++ Struct_Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 32;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
